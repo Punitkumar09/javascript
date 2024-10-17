ef6b4653f81a1497fc6aac4d92b7d36822240226
Punit Kumar <punitkumar4328@gmail.com>
2024-10-17 17:44:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "punit"
+
+console.log(typeof score)
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0
+
+let isLoggedIn = "Punit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false 
+// "punit" => true
+
+let someNumber =22 
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
