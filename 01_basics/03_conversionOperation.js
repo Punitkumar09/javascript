8895c3bacabad7debb4fdf44ba115992c9b6fe99
Punit Kumar <punitkumar4328@gmail.com>
2024-10-17 18:03:28 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,61 @@
-let score = "punit"
+// let score = "punit"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-//"33" => 33
-// "33abc" => NaN
-// ture => 1; false => 0
+// //"33" => 33
+// // "33abc" => NaN
+// // ture => 1; false => 0
 
-let isLoggedIn = "Punit"
+// let isLoggedIn = "Punit"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 => true; 0 => false
-// "" => false 
-// "punit" => true
+// // 1 => true; 0 => false
+// // "" => false 
+// // "punit" => true
 
-let someNumber =22 
+// let someNumber =22 
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// ******************************* Operations **********************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)  
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)   
+
+let str1 = "hello"
+let str2 = " punit"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log( "1" + 2)
+console.log(1 + "2")
+console.log("1" +2 +2)
+console.log(1+2+"2")
+
+console.log(true)
+console.log(+true)
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
