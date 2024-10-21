64bf3f5a186325e708c9984951ee011872cb1e40
Punit Kumar <punitkumar4328@gmail.com>
2024-10-21 18:10:38 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive datatypes
+// 7 types : String , Number , boolean , null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3489389489934389n
+
+console.log(bigNumber);
+
+
+// Reference (Non primitive)
+
+// Array , Objects , Functions
+
+const heros = ["Punit" , "Anuj" , "Mudit" , "Varun"];
+
+ let myObj = {
+    name: "Punit",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+console.log(typeof heros);
