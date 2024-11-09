f5c01ba12958bfde2c9dd23d80de4fad67700e3e
Punit Kumar <punitkumar4328@gmail.com>
2024-11-09 13:29:11 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "punit"
+const repoCount = 2
+
+//console.log(name + repoCount + "value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Punit')
+
+console.log(gameName[3]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+
