cad6f86251ea0ae6270817a6ca75e15e98dbf773
Punit Kumar <punitkumar4328@gmail.com>
2024-11-13 09:33:02 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,45 @@
+// Arrays
+
+const myArr = [0, 2 , 4 , 3 , 5 , 6]
+
+//console.log(myArr[1]);
+
+const myHeroes = ["Captain America" , "Iron Man"]
+
+// Array methods
+
+// myArr.push(7)
+// myArr.push(8)
+// myArr.pop()
+
+//myArr.unshift(9)
+//myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+
+// console.log( newArr);
+
+//slice , splice
+
+console.log("A", myArr);
+
+const myArr1 = myArr.slice(1 , 3)
+
+console.log(myArr1);
+
+console.log("b" , myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C" , myArr);
+console.log(myn2);
+
+// in slice function array will give same output 
+// in splice function array will give manipulate array
+
+
+
