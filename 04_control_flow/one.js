f9aa98f085a0b9228870faf5eaa804654703c351
Punit Kumar <punitkumar4328@gmail.com>
2024-11-16 11:42:29 +0000

    flow and control

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+//if
+
+// const isUserLoggedIn = true
+// const temperature = 40
+// if(temperature < 50){  // conditon true hogi to ander ka code execute hoga agr true nahi hai to nahi hoga ander ka code execote
+// //scope
+// console.log("less than 50")
+
+// }else
+// console.log("greater than 50")
+// <, > , <=, >=, ==, !=, ===, !==
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+// }
+// console.log(`user power : ${power}`);
+
+// shorthand notation
+
+const balance = 1000;
+if(balance > 500) console.log("test");  //implicit scope
+
+
